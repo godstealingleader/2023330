0f9f8bfd45efc1d253ad4dea3208f7e321c3e8e9
godstealingleader <107918254+godstealingleader@users.noreply.github.com>
2023-03-30 18:33:19 +0800

    Add files via upload

---
+++ 2023330/源.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	char name = 'paul';
+	int location = 3;
+	cout << "my name is: " << name << endl;
+	cout << "location is:" << location << endl;
+	return 0;
+}
